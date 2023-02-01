84f3ada537c0f7d0a97de12bb8b4e17bc3915044
Bakr Matlab <matlab893@gmail.com>
2023-02-01 06:24:41 -0500

    Added a full deck of cards that can be shuffled

--- javascript.js
+++ javascript.js
@@ -1,1 +1,48 @@
 // JavaScript file
+
+// Array of suits
+const suits = ["hearts", "diamonds", "spades", "clubs"];
+
+// Array of values
+const values = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
+
+// Class deck represents a deck of cards
+class Deck {
+  constructor() {
+    this.deck = [];
+    this.reset();
+  }
+
+  //Create a deck of cards
+  reset() {
+    this.deck = [];
+    for (let suit of suits) {
+      for (let value of values) {
+        this.deck.push(`${value} of ${suit}`);
+      }
+    }
+    return this;
+  }
+
+  //Shuffle the deck of cards
+  shuffle() {
+    let currentIndex = this.deck.length,
+      temporaryValue,
+      randomIndex;
+
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = this.deck[currentIndex];
+      this.deck[currentIndex] = this.deck[randomIndex];
+      this.deck[randomIndex] = temporaryValue;
+    }
+    return this;
+  }
+}
+
+// Create a new deck of cards
+const deck = new Deck();
+
+//Output the deck of cards
+console.log(deck.shuffle().deck);
