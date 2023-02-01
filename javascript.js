b41b91b10a1a9a781270c889028aeda5b143e7f3
Daniel Longo <long0266@algonquinlive.com>
2023-02-01 12:34:00 -0500

    progress on sorting cards from deck to tableau

--- javascript.js
+++ javascript.js
@@ -1,10 +1,21 @@
 // JavaScript file
 
+/* jslint es6:true */
+/* eslint-env es6 */
+/* eslint-disable */
+
 // Array of suits
 const suits = ["hearts", "diamonds", "spades", "clubs"];
 
 // Array of values
 const values = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
+
+// card object
+let card = {
+    suit: null,
+    value: null,
+    color: null
+}
 
 // Class deck represents a deck of cards
 class Deck {
@@ -17,8 +28,8 @@ class Deck {
   reset() {
     this.deck = [];
     for (let suit of suits) {
-      for (let value of values) {
-        this.deck.push(`${value} of ${suit}`);
+      for (let value of values) {      
+        this.deck.push(`${value}_of_${suit}`);
       }
     }
     return this;
@@ -46,3 +57,30 @@ const deck = new Deck();
 
 //Output the deck of cards
 console.log(deck.shuffle().deck);
+
+
+
+// TESTING
+
+const tableauOne = [];
+
+// take the last item from the array and put it into a card object
+card.value = deck.deck[deck.deck.length-1].split("_")[0];
+card.suit = deck.deck[deck.deck.length-1].split("_")[2];
+
+// identifies the card color
+if (card.suit == "diamonds" || card.suit == "hearts") {
+    card.color = "red";
+} else{
+    card.color = "black";
+}
+
+// send card to tableauOne
+tableauOne.push(card); 
+console.log(tableauOne);
+
+// changes the img src to the correct front image of the card
+document.getElementById("test").src = "images/"+deck.deck[deck.deck.length-1]+".png";
+
+// remove the card from the deck
+deck.deck.pop();
