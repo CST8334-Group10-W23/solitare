d8a34cfd6da01e42edab970f872a8616d47e0747
Daniel Longo <long0266@algonquinlive.com>
2023-02-02 11:47:31 -0500

    transform deck into stock pile

--- javascript.js
+++ javascript.js
@@ -11,11 +11,13 @@ const suits = ["hearts", "diamonds", "spades", "clubs"];
 const values = ["ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "jack", "queen", "king"];
 
 // card object
-let card = {
-    suit: null,
-    value: null,
-    color: null
-}
+//let card = {
+//    suit: null,
+//    value: null,
+//    color: null,
+//    frontImage: null,
+//    backImage: "images/deck_backing.jpg"
+//}
 
 // Class deck represents a deck of cards
 class Deck {
@@ -62,25 +64,88 @@ console.log(deck.shuffle().deck);
 
 // TESTING
 
+
+const p = stockPile();
+console.log(p);
+
+
+
+
+/*
 const tableauOne = [];
+const tableauTwo = [];
 
-// take the last item from the array and put it into a card object
-card.value = deck.deck[deck.deck.length-1].split("_")[0];
-card.suit = deck.deck[deck.deck.length-1].split("_")[2];
-
-// identifies the card color
-if (card.suit == "diamonds" || card.suit == "hearts") {
-    card.color = "red";
-} else{
-    card.color = "black";
-}
+// call set card function
+setCard(deck);
 
 // send card to tableauOne
 tableauOne.push(card); 
-console.log(tableauOne);
 
 // changes the img src to the correct front image of the card
-document.getElementById("test").src = "images/"+deck.deck[deck.deck.length-1]+".png";
+document.getElementById("test").src = card.frontImage;
 
 // remove the card from the deck
 deck.deck.pop();
+
+
+setCard(deck);
+tableauTwo.push(card);
+document.getElementById("test2").src = card.frontImage;
+deck.deck.pop();
+
+console.log(tableauOne);
+console.log(tableauTwo);
+console.log(deck.deck);
+
+*/
+
+
+// create card objects from the deck
+function setCard(deck) {
+    // take the last card from the deck and gather details
+    value = deck.deck[deck.deck.length-1].split("_")[0];
+    suit = deck.deck[deck.deck.length-1].split("_")[2];
+    frontImage = "images/"+deck.deck[deck.deck.length-1]+".png";
+
+    // diamonds or hearts is a red card color
+    if (suit == "diamonds" || suit == "hearts") {
+        color = "red";
+    } 
+    // spades and clubs is a black card color
+    else {
+        color = "black";
+    }
+    
+    // create card object with the details
+    let card = {
+        value: value,
+        suit: suit,
+        color: color,
+        frontImage: frontImage,
+        backImage: "images/deck_backing.jpg"
+    }
+    
+    // remove the last card from the deck
+    deck.deck.pop();
+    
+    // return the card object
+    return card;
+}
+
+// turn deck into the stock pile
+function stockPile() {
+    // an array for the stock 
+    const stock = [];
+    
+    // loop through all the cards in the deck
+    for (let i=0; i < 52; i++) {
+
+        // make each card into a card object
+        let card = setCard(deck);
+        
+        // put the card object into the stock array
+        stock.push(card);
+    }
+    // return the stock array
+    return stock;
+}
